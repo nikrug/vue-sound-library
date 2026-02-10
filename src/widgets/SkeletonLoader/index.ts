d67f86461d2f28c8bad64229a945572422e5e9c2
Никита Домрачев <domrachev-nikitka@mail.ru>
2026-02-10 15:07:44 +0700

    /feature:попытка выведения db.json на netify

--- src/widgets/SkeletonLoader/index.ts
+++ src/widgets/SkeletonLoader/index.ts
@@ -1,6 +1,8 @@
-import SkeletonLoader from 'src/widgets/SkeletonLoader/ui/SkeletonLoader.vue'
+import pizzas from 'db.json'; 
+
+import SkeletonLoader from 'src/widgets/SkeletonLoader/ui/SkeletonLoader.vue';
 
 export {
   SkeletonLoader,
-  
+  pizzas,
 };
